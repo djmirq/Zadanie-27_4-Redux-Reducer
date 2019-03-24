9340683aa3a289ee525ec9315d3d1baf5e256c0c
djmirq <36812301+djmirq@users.noreply.github.com>
2019-03-24 15:42:15 +0100

    Fix: all files

--- action.js
+++ action.js
@@ -1,8 +1,67 @@
-import { combineReducers } from "redux";
-import comments from "./comments";
-import users from "./users";
+import uuid from uuid;
 
-const app = combineReducers({
-  comments,
-  users
-});
+export const ADD_COMMENT = 'ADD_COMMENT';
+export const REMOVE_COMMENT = 'REMOVE_COMMENT';
+export const EDIT_COMMENT = 'EDIT_COMMENT';
+export const THUMB_UP_COMMENT = 'THUMB_UP_COMMENT';
+export const THUMB_DOWN_COMMENT = 'THUMB_DOWN_COMMENT';
+
+function addComment(text) {
+	return {
+		type: ADD_COMMENT,
+		text,
+		id: uuid.v4()
+	}
+}
+
+function removeComment(id) {
+	return {
+		type: REMOVE_COMMENT,
+		id
+	}
+}
+
+function editComment(id, text) {
+	return {
+		type: EDIT_COMMENT,
+		id,
+		text
+	}
+}
+
+function thumbUpComment(id) {
+	return {
+		type: THUMB_UP_COMMENT,
+		id
+	}
+}
+
+function thumbDownComment(id) {
+	return {
+		type: THUMB_DOWN_COMMENT,
+		id
+	}
+}
+ 
+function rateComment(id, bool) {
+	return {
+		type: bool ? THUMB_UP_COMMENT : THUMB_DOWN_COMMENT,
+		id
+	}
+}
+
+function rateComment_2(id,bool) {
+	return {
+		id,
+		type: `THUMB_${bool?'UP':'DOWN'}_COMMENT`
+	}
+}
+
+ 
+const boundRateComment = (id,bool,stringMethod) => stringMethod ? rateComment_2(id,bool) : dispatch(rateComment(id, bool));
+ 
+const boundAddComment = text => dispatch(addComment(text));
+const boundRemoveComment = id => dispatch(removeComment(id));
+const boundEditComment = (id, text) => dispatch(editComment(id, text));
+const boundThumbUpComment = id => dispatch(thumbUpComment(id));
+const boundThumbDownComment = id => dispatch(thumbDownComment(id));
--- comments.js
+++ comments.js
@@ -1,65 +1,44 @@
-
-import { ADD_COMMENT } from './action';
-import { EDIT_COMMENT} from './action';
-import { REMOVE_COMMENT} from './action';
-import { THUMB_UP_COMMENT} from './action';
-import { THUMB_DOWN_COMMENT} from './action';
-
-const initialState = {
-    comments: [],
-    users =[]
-};
-
-function comments(state = [], action) {
-    switch (action.type) {
-        case ADD_COMMENT:
-            return [{
-                id: action.id,
-                text: action.text,
-                votes: 0
+export function comments(state = [], action) {
+  switch (action.type) {
+    case ADD_COMMENT:
+      return [
+        {
+          id: action.id,
+          text: action.text,
+          votes: 0
+        },
+        ...state
+      ];
+    case REMOVE_COMMENT:
+      return state.filter(comment => comment.id !== action.id);
+    case EDIT_COMMENT:
+      return state.map(comment =>
+        comment.id === action.id
+          ? {
+              ...comment,
+              ...action
             }
-                , ...state.comments];
-
-        case REMOVE_COMMENT:
-            return state.filter(comment => comment.id !== action.id);
-            
-
-        case EDIT_COMMENT:
-            return state.map(comment => {
-                if (comment.id === action.id) {
-                    return {
-                        ...comment,
-                        text: action.text
-                    }
-                }   
-                return comment;
-            });
-            
-        case THUMB_UP_COMMENT:
-            return state.map(comment => {
-                if (comment.id === action.id) {
-                    return {
-                        ...comment,
-                        votes: comment.votes + 1
-                    }
-                }    
-                return comment;
-            });
-
-        case THUMB_DOWN_COMMENT:
-            return state.map(comment => {
-                if (comment.id === action.id) {
-                    return {
-                        ...comment,
-                        votes: comment.votes -1
-                    }
-                }  
-                return comment;
-            });
-
-        default:
-            return state;
-    }
+          : comment
+      );
+    case THUMB_UP_COMMENT:
+      return state.map(comment =>
+        comment.id === action.id
+          ? {
+              ...comment,
+              votes: comment.votes + 1
+            }
+          : comment
+      );
+    case THUMB_DOWN_COMMENT:
+      return state.map(comment =>
+        comment.id === action.id
+          ? {
+              ...comment,
+              votes: comment.votes + 1
+            }
+          : comment
+      );
+    default:
+      return state;
+  }
 }
-
-export default comments;
